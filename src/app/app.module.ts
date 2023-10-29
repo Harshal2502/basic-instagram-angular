77a1c16c6afc334c5caf8bb45c50dcdd0b1756a0
Harshal Devmurari <92778686+Harshal2502@users.noreply.github.com>
2023-10-29 14:37:03 +0530

    Angular-forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -33,6 +33,7 @@ import { AlertComponent } from './shared/alert/alert.component';
     BrowserModule,
     NgbModule,
     HttpClientModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastService } from '../services/toast-service.service';
 import { LoginService } from '../services/loginservice.service';
 import { USER_ALERTS } from '../utils/constants';
@@ -11,32 +12,28 @@ import { CookiesService } from '../coockies.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
-  err1: Boolean = false;
-  err2: Boolean = false;
+  loginForm: FormGroup;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
     private cookies: CookiesService,
-    private toast: ToastService
-  ) {}
+    private toast: ToastService,
+    private fb: FormBuilder, // Inject FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   async login() {
-    if (this.username == '') {
-      this.err1 = true;
-      return;
-    }
-
-    if (this.password == '') {
-      this.err2 = true;
-      return;
-    }
-
-    if (this.username != '' && this.password != '') {
+    if (this.loginForm.valid) {
       try {
-        const res = await this.loginService.login(this.username, this.password);
+        const res = await this.loginService.login(
+          this.loginForm.value.username,
+          this.loginForm.value.password
+        );
 
         if (res?.userId !== null) {
           this.cookies.setAuthToken(res.authToken);
@@ -50,14 +47,8 @@ export class LoginComponent {
           this.toast.showInfo(USER_ALERTS.USER_NOT_EXISTS);
         if (err.status === 401) this.toast.showInfo(USER_ALERTS.WRONG_PASSWORD);
       }
+    } else {
+      this.loginForm.markAllAsTouched();
     }
-  }
-
-  onUsernameInput() {
-    this.err1 = false;
-  }
-
-  onPasswordInput() {
-    this.err2 = false;
   }
 }
